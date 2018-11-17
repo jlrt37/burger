95f51b1ec1b0d0891cb102cdc6186f28f8602915
jlrt37 <joshua.l.ragon@gmail.com>
2018-11-17 15:32:07 -0600

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('No Burger', true);
