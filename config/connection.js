3b6ef8c7687cc664728fe5b2eae28b4ebdab1101
jlrt37 <joshua.l.ragon@gmail.com>
2018-11-19 21:11:25 -0600

    edit

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,11 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
+		port: 3306,
 		host: 'localhost',
 		user: 'root',
-		password: 'hacktheplanet',
-		database: 'todoagain_db'
+		password: 'Hnvqwf12!',
+		database: 'burgers_db'
 	});
 }
 
